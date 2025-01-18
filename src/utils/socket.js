3dde8524d9a3a7949e01dc23cb63f1e2d3669e94
navneet21bcs <navneet9876543@gmail.com>
2025-01-19 01:32:28 +0530

    socket changes1

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,6 +5,7 @@ export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
     return io(BASE_URL);
   } else {
-    return io("/", { path: "/api/socket.io" });
+    // return io("/", { path: "/api/socket.io" });
+    return io("https://devtinder-backend-3-2bt5.onrender.com");
   }
 };
